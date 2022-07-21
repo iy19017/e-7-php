88e1d9f8e755a0156c26ef917dfa4b15c6ab1841
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-07-21 14:37:00 +0900

    Create PageB.php

---
+++ PageB.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <meta charset = "utf-8">
+</head>
+
+<body>
+
+  <?php
+  
+    $shohin = $_GET['name'];
+    
+    $kingaku = $_GET['money'];
+    
+    print $shohin $kingaku;  
+  
+  ?>
+
+</body
+</html>
