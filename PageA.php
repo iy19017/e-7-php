2caca6a599479cab92dd038d4ffcce71346d9b49
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-07-21 14:06:43 +0900

    Create PageA.php

---
+++ PageA.php
@@ -0,0 +1,10 @@
+<?php
+
+  <form action = "PageB.php" method = "get">
+    <input type = "text" name = "name" value = "" size = "20" maxlength = "20"><br>
+    <input type = "text" name = "money" value = "" size = "20" maxlength = "20"><br>
+    <input type = "submit" value = "送信">
+    <input type = "clear" value = "クリア">
+  </form>
+    
+?>
