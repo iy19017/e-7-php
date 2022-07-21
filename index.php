12f5b26c84bf38cd704698abdf4258bc746e7e25
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-07-21 14:19:02 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
-  <form action = "PageB.php" method = "get">
-    <input type = "text" name = "name" value = "" size = "20" maxlength = "20"><br>
-    <input type = "text" name = "money" value = "" size = "20" maxlength = "20"><br>
-    <input type = "submit" value = "送信">
-    <input type = "clear" value = "クリア">
-  </form>
+  <head>
+    <meta charset = "utf-8">
+  </head>
+    
+  <body>
+    <form action = "PageB.php" method = "get">
+      <input type = "text" name = "name" value = "" size = "20" maxlength = "20"><br>
+      <input type = "text" name = "money" value = "" size = "20" maxlength = "20"><br>
+      <input type = "submit" value = "送信">
+      <input type = "clear" value = "クリア">
+    </form>
+  </body>
     
 ?>
