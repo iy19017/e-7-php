76b9121c4702593276182017807ec14aae8d716a
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-07-21 15:29:13 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -28,7 +28,17 @@
   
   function add(){
     
-    <a href = "index.php">
+    <?php
+    
+    $server = "ec2-52-48-159-67.eu-west-1.compute.amazonaws.com";
+    $database = "d8bgma6jnt8fbd";
+    $port_number = 5432;
+    $user_id = "qcbzuxmlmaijby";
+    $user_password = "bb16bd25fb0ee94746f9270dc541f7e3d8d1c2a9eeeb76b299795c9de6b80aab"; 
+    
+    $connect = new PDO("pgsql:host=$server; dbname=$database; port=$port_number; user=$user_id; password=$user_password");
+    
+    INSERT INTO "order" (商品名、価格) VALUES('$shohin', $kingaku)
     
   }
   
