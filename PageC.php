a0f11e153711bcacffdc8b69860b73994b47cc23
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-08 15:43:13 +0900

    Update PageC.php

--- PageC.php
+++ PageC.php
@@ -6,7 +6,6 @@
     
     <?php
         
-  
       $server = "ec2-52-48-159-67.eu-west-1.compute.amazonaws.com";
       $database = "d8bgma6jnt8fbd";
       $port_number = 5432;
@@ -25,24 +24,23 @@
       print "<th>turnover</th>"; // 表の見出し turnover
       print "</tr>";　　　　　　 // 表の見出し行の終わり
   
-      while (($row = pg_fetch_array ($result))):
-        print "<tr>";　　　　　　 // 表の明細行の始まり
-        print ’<td>’.$row[0]."</td>";
-        print ’<td>’.$row[1]."</td>";
-        print "</tr>Y=n";　　　　　　 // 表の明細行の終わり
-      endwhile;
+//       while (($row = pg_fetch_array ($result))):
+//         print "<tr>";　　　　　　 // 表の明細行の始まり
+//         print ’<td>’.$row[0]."</td>";
+//         print ’<td>’.$row[1]."</td>";
+//         print "</tr>Y=n";　　　　　　 // 表の明細行の終わり
+//       endwhile;
 
-      $rs = $result->fetchAll ();
-      foreach ($rs as $row):
-        print "<tr>";　　　　　　 // 表の明細行の始まり
-        print ’<td>’.$row[’商品名’]."</td>";
-        print ’<td>’.$row[’価格’]."</td>";
-        print ’<td>’.$row[’登録日’]."</td>";
-        print "</tr>Y=n";　　　　　　 // 表の明細行の終わり
-      endforeach;
-      print "</table>Y=n";　　　　　　 // 表の終わり
+//       $rs = $result->fetchAll ();
+//       foreach ($rs as $row):
+//         print "<tr>";　　　　　　 // 表の明細行の始まり
+//         print ’<td>’.$row[’商品名’]."</td>";
+//         print ’<td>’.$row[’価格’]."</td>";
+//         print ’<td>’.$row[’登録日’]."</td>";
+//         print "</tr>Y=n";　　　　　　 // 表の明細行の終わり
+//       endforeach;
+//       print "</table>Y=n";　　　　　　 // 表の終わり
 
-  
     ?> 
 
 </html>
