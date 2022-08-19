bb78c97f400090207890cd83ce564f52d25799fe
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-19 19:28:46 +0900

    Update PageC.php

--- PageC.php
+++ PageC.php
@@ -15,39 +15,32 @@
       $connect = new PDO("pgsql:host=$server; dbname=$database; port=$port_number;　user=$user_id; password=$user_password");
   
      $result = $connect->query("SELECT * FROM order");
-      foreach($results as $row)
-      {
-        ?><?=row["商品名"]?> : <?=row["価格"]?>: <?=row["登録日"]?><br>
-        <?php
-      }
   
 //       $result = &connect -> query(&sql_text);
   
-//       print "<h3>データベース検索結果</h3>\=n";
+      print "<h3>データベース検索結果</h3>\=n";
   
-//       print "<table border=1 cellspacing=1 cellpadding=1>\n";
-//       print "<tr>";　　 // 表の見出し行の始まり
-//       print "<th>company</th>"; // 表の見出し company
-//       print "<th>turnover</th>"; // 表の見出し turnover
-//       print "<th>data</th>";
-//       print "</tr>";　　　　　　 // 表の見出し行の終わり
+      print "<table border=1 cellspacing=1 cellpadding=1>\n";
+      print "<tr>";　　 // 表の見出し行の始まり
+      print "<th>company</th>"; // 表の見出し company
+      print "<th>turnover</th>"; // 表の見出し turnover
+      print "</tr>";　　　　　　 // 表の見出し行の終わり
   
-//       while (($row = pg_fetch_array ($result))):
-//         print "<tr>";　　　　　　 // 表の明細行の始まり
-//         print ’<td>’.$row[0]."</td>";
-//         print ’<td>’.$row[1]."</td>";
-//         print "</tr>Y=n";　　　　　　 // 表の明細行の終わり
-//       endwhile;
+      while (($row = pg_fetch_array ($result))):
+        print "<tr>";　　　　　　 // 表の明細行の始まり
+        print ’<td>’.$row[0]."</td>";
+        print ’<td>’.$row[1]."</td>";
+        print "</tr>Y=n";　　　　　　 // 表の明細行の終わり
+      endwhile;
 
-//       $rs = $result->fetchAll ();
-//       foreach ($rs as $row):
-//         print "<tr>";　　　　　　 // 表の明細行の始まり
-//         print ’<td>’.$row[’商品名’]."</td>";
-//         print ’<td>’.$row[’価格’]."</td>";
-//         print ’<td>’.$row[’登録日’]."</td>";
-//         print "</tr>\=n";　　　　　　 // 表の明細行の終わり
-//       endforeach;
-//       print "</table>\=n";　　　　　　 // 表の終わり
+      $rs = $result->fetchAll ();
+      foreach ($rs as $row):
+        print "<tr>";　　　　　　 // 表の明細行の始まり
+        print ’<td>’.$row[’商品名’]."</td>";
+        print ’<td>’.$row[’価格’]."</td>";
+        print "</tr>\=n";　　　　　　 // 表の明細行の終わり
+      endforeach;
+      print "</table>\=n";　　　　　　 // 表の終わり
   
     ?> 
 
