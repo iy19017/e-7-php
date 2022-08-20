59a4a980e6a27b4db1b5414b23f0b62bad3f5e8f
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-20 23:59:05 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -1,3 +1,13 @@
+<?php
+	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
+      　$database = "dao0aqgjli0ekl";
+      　$port_number = 5432;
+      　$user_id = "njtersecjvwybh";
+      　$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
+
+	$connect = new PDO("pgsql:host=$server; dbname=$database; port=$port_number;　user=$user_id; password=$user_password");
+ ?>
+
 <html>
   
 <head>
@@ -13,18 +23,8 @@
   
   <?php	
   if(isset($_POST['select_evaluation_yes'])) {
-	print("Button Click");
-// 	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
-//       　$database = "dao0aqgjli0ekl";
-//       　$port_number = 5432;
-//       　$user_id = "njtersecjvwybh";
-//       　$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
-
-//       　$connect = new PDO("pgsql:host=$server; dbname=$database; port=$port_number;　user=$user_id; password=$user_password");
-	
-// 	$today = date("Y-m-d");
-// 	$query = "INSERT INTO users(item,price,day) VALUES('$shohin','$kingaku','$today')";
-// 	header("location: index.php");
+	$today = date("Y-m-d");
+	$query = "INSERT INTO users(item,price,day) VALUES('$shohin','$kingaku','$today')";
   }
   ?>  
   </body>
