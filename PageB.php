209e8136a5cdd8949285a7d0a47bfe6e0a56b989
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-21 00:31:42 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -1,11 +1,11 @@
 <?php
-	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
-      　$database = "dao0aqgjli0ekl";
-      　$port_number = 5432;
-      　$user_id = "njtersecjvwybh";
-      　$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
+// 	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
+//       　$database = "dao0aqgjli0ekl";
+//       　$port_number = 5432;
+//       　$user_id = "njtersecjvwybh";
+//       　$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
 
-	$connect = new PDO("pgsql:host=$server; dbname=$database; port=$port_number;　user=$user_id; password=$user_password");
+// 	$connect = new PDO("pgsql:host=$server; dbname=$database; port=$port_number;　user=$user_id; password=$user_password");
  ?>
 
 <html>
@@ -15,11 +15,16 @@
 </head>
   <body>
       <?php
-	$shohin = $_POST
+	$shohin = $_POST['name'];
+	$kingaku = $_POST['money'];
+	print("$shohin $kingaku");
       ?>
   <br>
-　<button type="button" onclick="click_yes" name="select_evaluation_yes" value="よかった">
-　<button type="button" name="select_evaluation_no" value="わるかった" href="index.php">
+	  
+  <form action ="index.php" method = "POST">
+	 <input type="button" onclick="click_yes" name="select_evaluation_yes" value="よかった">
+	 <input type="button" name="select_evaluation_no" value="わるかった" href="index.php">
+  </form>
   
   <?php	
   if(isset($_POST['select_evaluation_yes'])) {
