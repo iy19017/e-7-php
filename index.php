b5538ce763fa63e971cd1db06fd370c7384e18f0
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-21 01:19:49 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
   <head>
       <meta charset = "utf-8">
@@ -14,13 +14,3 @@
   </body>
 
 </html>
- -->
-<?php
- $server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
-      　$database = "dao0aqgjli0ekl";
-      　$port_number = 5432;
-      　$user_id = "njtersecjvwybh";
-      　$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
-
-	$connect = new PDO("pgsql:host=$server; dbname=$database; port=$port_number;　user=$user_id; password=$user_password");
-?>
