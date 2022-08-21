415c64fe0c21b11af7e723e9c4f4c3b4a4861d9d
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-22 02:06:11 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -44,7 +44,7 @@
   print("接続");
   
   $today = date("Y-m-d");
-  $query = 'INSERT INTO(item,price,day) VALUES(:shohin,:kingaku,:today)';	  
+  $query = $pdo->prepare('INSERT INTO(item,price,day) VALUES(:shohin,:kingaku,:today)');	  
 	  
   if(isset($_POST['select_evaluation_yes'])) {
 // 	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
