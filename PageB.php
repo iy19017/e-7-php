7c46ae425bd1643f7354f7ada61c8c385f6bb8c4
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-22 03:39:04 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -24,7 +24,7 @@
   </form>
   
   <?php
-//   try{
+  try{
 	  
   	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
         $database = "dao0aqgjli0ekl";
@@ -33,12 +33,24 @@
 	$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
 
 	$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");
-  
-//   } catch(PDOException $e){
 	  
-//     print("ERROR");
+	$today = date("Y-m-d");
 	  
-//   }
+	$query = $pdo->prepare('INSERT INTO order(item,price,day) VALUES(:shohin,:kingaku,:today)');
+
+	$query->bindValue(':item', $POST['name'], PDO::PARAM_STR);
+
+	$query->bindValue(':price', $POST['value'], PDO::PARAM_INT);]
+
+	$query->bindValue(':day', $today, PDO::PARAM_STR);
+
+	$query->execute();
+
+  } catch(PDOException $e){
+	  
+    print("ERROR");
+	  
+  }
   
   $today = date("Y-m-d");
 	  
@@ -52,12 +64,12 @@
    
   $query->execute();
 	
-  if(isset($_POST['select_evaluation_yes'])) {
+//   if(isset($_POST['select_evaluation_yes'])) {
 	  
 // 	$today = date("Y-m-d");
 // 	$query = 'INSERT INTO(item,price,day) VALUES(:shohin,:kingaku,:today)';
 
-  }
+//   }
   ?>  
   </body>
   
