82f6d49a726a1d79e3937b9921b9493102ea2233
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-22 00:05:38 +0900

    Update index.php

--- index.php
+++ index.php
@@ -12,18 +12,4 @@
         <input type = "reset" value = "クリア">
       </form>
   </body>
-
-  <?php
-// 	$shohin = $_POST['name'];
-// 	$kingaku = $_POST['money'];
-// 	print("$shohin $kingaku");
-	  
-	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
-      　$database = "dao0aqgjli0ekl";
-      　$port_number = 5432;
-      　$user_id = "njtersecjvwybh";
-      　$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
-
-	$connect = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");
-      ?>
 </html>
