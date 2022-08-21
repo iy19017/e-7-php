0769c350a6e02d6eaf4d7a8bacd108495d231edf
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-22 01:00:44 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
   <head>
       <meta charset = "utf-8">
@@ -12,4 +12,20 @@
         <input type = "reset" value = "クリア">
       </form>
   </body>
-</html>
+</html> -->
+
+<?php
+
+  print("接続前");
+
+  $server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
+  $database = "dao0aqgjli0ekl";
+  $host = 5432;
+	$user_id = "njtersecjvwybh";
+	$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
+
+	$pdo = new PDO("pgsql:host=$server; dbname=$database; port=$port_number; user=$user_id; password=$user_password");  
+  
+  print("接続後");
+
+?>
