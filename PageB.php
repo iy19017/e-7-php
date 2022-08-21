3ffb1a2f1859576524bcf0b865304df7967721cd
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-22 07:43:23 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -17,22 +17,6 @@
   </form>
   
   <?php
-  try{
-	  
-  	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
-        $database = "dao0aqgjli0ekl";
-        $host = 5432;
-	$user_id = "njtersecjvwybh";
-	$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
-
-	$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");
-	  
-  } catch(PDOException $e){
-	  
-    print("ERROR");
-	  
-  }
-	
   if(isset($_POST['yes'])) {
 	  
 	echo "PUSH YES";  
@@ -58,6 +42,8 @@
 	$stmt -> bindParam(":today", $today);
 		
 	$stmt -> execute();
+		
+	echo ("終了");
 		
 	header("Location: ./index.php") ;
 	  
