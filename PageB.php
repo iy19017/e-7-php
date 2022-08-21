36d2a45d93bd73a017a21b8cbd79f53257cd483d
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-22 03:53:13 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -4,53 +4,45 @@
 </head>
   <body>
       <?php
-// 	$shohin = $_POST['name'];
-// 	$kingaku = $_POST['money'];
-// 	print("$shohin $kingaku");
+	$shohin = $_POST['name'];
+	$kingaku = $_POST['money'];
+	print("$shohin $kingaku");
 	  
-// 	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
-//         $database = "dao0aqgjli0ekl";
-//         $host = 5432;
-// 	$user_id = "njtersecjvwybh";
-// 	$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
-
-// 	$pdo = new PDO("pgsql:host=$server; dbname=$database; port=$port_number; user=$user_id; password=$user_password");
-      
 	?>
-  <br>  
+  <br>
   <form method = "POST">
-	 <input type="button" onclick="click_yes" name="select_evaluation_yes" value="よかった">
-	 <input type="button" name="select_evaluation_no" value="わるかった" href="index.php">
+	 <input type="button" name="select_evaluation_yes" value="よかった">
+	 <input type="button" name="select_evaluation_no" value="わるかった">
   </form>
   
   <?php
-//   try{
+  try{
 	  
-//   	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
-//         $database = "dao0aqgjli0ekl";
-//         $host = 5432;
-// 	$user_id = "njtersecjvwybh";
-// 	$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
+  	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
+        $database = "dao0aqgjli0ekl";
+        $host = 5432;
+	$user_id = "njtersecjvwybh";
+	$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
 
-// 	$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");
+	$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");
 	  
-// 	$today = date("Y-m-d");
+	$today = date("Y-m-d");
 	  
-// 	$query = $pdo->prepare("INSERT INTO order(item,price) VALUES(:shohin,:kingaku)");
+	$query = $pdo->prepare("INSERT INTO order (item,price) VALUES(:shohin,:kingaku)");
 
-// 	$query->bindParam(':item', $POST['name'], PDO::PARAM_STR);
+	$query->bindParam(':item', $POST['name'], PDO::PARAM_STR);
 
-// 	$query->bindValue(':price', $POST['value'], PDO::PARAM_INT);]
+	$query->bindValue(':price', $POST['value'], PDO::PARAM_INT);]
 
-// 	$query->bindParam(':day', $today, PDO::PARAM_STR);
+	$query->bindParam(':day', $today, PDO::PARAM_STR);
 
-// 	$query->execute();
+	$query->execute();
 
-//   } catch(PDOException $e){
+  } catch(PDOException $e){
 	  
-//     print("ERROR");
+    print("ERROR");
 	  
-//   }
+  }
   
 //   $today = date("Y-m-d");
 	  
