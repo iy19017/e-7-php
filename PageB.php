1d5ee595f214a544be2d5afeeaedfd2e20584dbf
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-22 03:03:55 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -33,36 +33,25 @@
 
 	$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");
   
-  	print("NNN");
-	  
-	
-  
   } catch(PDOException $e){
     print("ERROR");
   }
   
-  print("接続");
-  
   $today = date("Y-m-d");
-  $query = "INSERT INTO order(item,price,day) VALUES(:shohin,:kingaku,:today)";
+	  
+  $query = 'INSERT INTO order(item,price,day) VALUES(:shohin,:kingaku,:today)';
    
   $result = pg_query($pdo, $query);
 	  
-  print("追加");	  
-	  
   if(isset($_POST['select_evaluation_yes'])) {
-// 	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
-//      $database = "dao0aqgjli0ekl";
-//      $host = 5432;
-// 	$user_id = "njtersecjvwybh";
-// 	$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
-
-// 	$pdo = new PDO("pgsql:host=$server; dbname=$database; port=$port_number; user=$user_id; password=$user_password");
 	
+	echo ("");
+	  
 // 	$today = date("Y-m-d");
 // 	$query = 'INSERT INTO(item,price,day) VALUES(:shohin,:kingaku,:today)';
 	  
 // 	$pdo = null;
+	  
   }
   ?>  
   </body>
