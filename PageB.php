dd5791e7d97cef6646ea805515da148a12ed255a
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-21 23:49:25 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -5,17 +5,17 @@
 </head>
   <body>
       <?php
-// 	$shohin = $_POST['name'];
-// 	$kingaku = $_POST['money'];
-// 	print("$shohin $kingaku");
+	$shohin = $_POST['name'];
+	$kingaku = $_POST['money'];
+	print("$shohin $kingaku");
 	  
-// 	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
-//       　$database = "dao0aqgjli0ekl";
-//       　$port_number = "5432";
-//       　$user_id = "njtersecjvwybh";
-//       　$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
+	$server = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com";
+      　$database = "dao0aqgjli0ekl";
+      　$port_number = 5432;
+      　$user_id = "njtersecjvwybh";
+      　$user_password = "b50e46eb2f6767eb9e27889330ef5ec058168f2c0abed5610dd08a2f9c61b694"; 
 
-// 	$connect = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");
+	$connect = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");
       ?>
   <br>  
   <form method = "POST">
