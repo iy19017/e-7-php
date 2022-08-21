66855985b7f8358c76518cdf79586453f4e1d2ec
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-22 01:58:40 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -55,7 +55,7 @@
 // 	$pdo = new PDO("pgsql:host=$server; dbname=$database; port=$port_number; user=$user_id; password=$user_password");
 	
 	$today = date("Y-m-d");
-	$query = "INSERT INTO(item,price,day) VALUES(fdsafdasadsf,64,3737)";
+	$query = 'INSERT INTO(item,price,day) VALUES(:shohin,:kingaku,:today)';
 	  
 // 	$pdo = null;
   }
