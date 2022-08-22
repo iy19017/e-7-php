d7cac70fce5080b67af7c3b2d1d3a97625506206
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 04:55:33 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -23,7 +23,7 @@
 	try{
 	  
 		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
-		$database = "dcrb5clh0jqmke";
+		$database = "dcrfa411341431b5clh0jqmke";
 		$host = 5432;
 		$user_id = "owlsbojfhnbkxz";
 		$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
