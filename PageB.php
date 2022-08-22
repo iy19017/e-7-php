37c7bacc824662c52b492a70a906829f2cca661b
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 03:01:16 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -22,7 +22,7 @@
   <?php
   if(isset($_POST['yes'])) {
 	
-	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES (':shohin', :kingaku, :today)";
+	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES (':shohin', :kingaku, ':today')";
 	  
 	echo $sql;
 	  
