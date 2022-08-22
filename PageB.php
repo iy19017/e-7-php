6d9833abed3ec2cebd34b0e331689fa0fb42e724
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 05:03:42 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -20,23 +20,23 @@
 
   if(isset($_POST['yes'])) {
 	  
-	try{
+// 	try{
 	  
-		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
-		$database = "dcrb5clh0jqmke";
-		$host = 5432;
-		$user_id = "owlsbojfhnbkxz";
-		$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
+// 		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
+// 		$database = "dcrb5clh0jqmke";
+// 		$host = 5432;
+// 		$user_id = "owlsbojfhnbkxz";
+// 		$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
 
-		$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");	
+// 		$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");	
 
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	} catch(PDOException $e){
+// 	} catch(PDOException $e){
 
-		echo "ERROR";
+// 		echo "ERROR";
 
-	}
+// 	}
 	  
 	$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
 	$database = "dcrb5clh0jqmke";
@@ -54,17 +54,25 @@
 	
 	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES (':shohin', :kingaku, ':today')";
 	  
-	$stmt = $pdo->prepare($sql);
+	$result = pg_query($conn, $sql);
 	  
-	$stmt->bindParam(':shohin', $shohin, PDO::PARAM_STR);
+	if(!$result){
+		
+		echo "ERROR2";
+		
+	}
+	  
+// 	$stmt = $pdo->prepare($sql);
+	  
+// 	$stmt->bindParam(':shohin', $shohin, PDO::PARAM_STR);
 
-	$stmt->bindParam(':kingaku', $kingaku, PDO::PARAM_INT);
+// 	$stmt->bindParam(':kingaku', $kingaku, PDO::PARAM_INT);
 
-	$stmt->bindParam(':today', $today, PDO::PARAM_STR);
+// 	$stmt->bindParam(':today', $today, PDO::PARAM_STR);
 	  
-	$stmt->execute();
+// 	$stmt->execute();
 	  
-	$result = $pdo->query ($sql);
+// 	$result = $pdo->query ($sql);
 	  
   }
 	  
