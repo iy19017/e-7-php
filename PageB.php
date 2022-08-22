a810f59f148296dc33f0df2b1d0c6ccb25afe8ba
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 04:57:42 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -32,21 +32,25 @@
 
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// 		$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
 	} catch(PDOException $e){
 
 		echo "ERROR";
 
 	}
 	  
-// 	$conn = ("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");
+	$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
+	$database = "dcrb5clh0jqmke";
+	$host = 5432;
+	$user_id = "owlsbojfhnbkxz";
+	$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
 	  
-// 	if(!$conn){
+	$conn = ("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");
+	  
+	if(!$conn){
 	
-// 		echo "ERROR";
+		echo "ERROR";
 		
-// 	}
+	}
 	
 	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES (':shohin', :kingaku, ':today')";
 	  
