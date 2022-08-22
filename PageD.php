972ec9f92799810289f99233973b784be01399d6
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-22 23:31:34 +0900

    Update PageD.php

--- PageD.php
+++ PageD.php
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset = "utf-8">
+  </head>
+  
+  <body>
+    
+    try{
+	  
+      $server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
+      $database = "dcrb5clh0jqmke";
+      $host = 5432;
+      $user_id = "owlsbojfhnbkxz";
+      $user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
 
+      $pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");	
+
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    
+      echo "OK";
+
+	} catch(PDOException $e){
+
+		print("ERROR");
+
+	}
+    
+  </body>
+</html>
