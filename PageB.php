6f822483310d01a6f416e8f054ff73b237e08f9c
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 02:54:23 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -47,7 +47,7 @@
 	  
   if(isset($_POST['yes'])) {
 	
-	$sql = "INSERT INTO ¥"order¥" (商品名, 価格, 登録日) VALUES (':shohin', :kingaku, :today)";
+	$sql = "INSERT INTO order (商品名, 価格, 登録日) VALUES (':shohin', :kingaku, :today)";
 	  
 	$stmt = $pdo->prepare($sql);
 	  
