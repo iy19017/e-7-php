4052f2ac9ce56bfbf539b23311596d4fb46a22f6
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 21:30:51 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset = "utf-8">
+  </head>
+    
+  <body>
+    
+    <a href="input.php">入力</a>
+    
+    <a href="serch.php">検索</a>
+    
+  </body>
+</html>
