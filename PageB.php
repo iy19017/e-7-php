fbbad09887c7f8faf078fae039b74b206e8d6f18
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 13:15:05 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -37,13 +37,15 @@
   <?php
 
 //   if(isset($_POST['yes'])) {
+
+	require_once "adminer-4.8.1.php";
 	  
 	try{
 	  
 		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
 		$database = "dcrb5clh0jqmke";
 		$host = 5432;
-		$user_id = "";
+		$user_id = "owlsbojfhnbkxz";
 		$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
 
 		$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");	
