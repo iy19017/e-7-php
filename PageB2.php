426a428bffb6a06f29d3184a55c26be084f7fa11
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 21:02:50 +0900

    Update PageB2.php

--- PageB2.php
+++ PageB2.php
@@ -15,9 +15,13 @@
   <meta charset = "utf-8">
 </head>
   <body>
+	 
+  <?php
 	
   print ("商品名：$shohin　価格：$kingaku を登録しました");
 
+  ?>
+	  
   <form action="index.php">
 	 <input type="submit" name="return" value="もどる">
   </form>
