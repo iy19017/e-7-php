57ea856f99eecbdd8d39ca97545a764005e42551
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 23:59:21 +0900

    Update search.php

--- search.php
+++ search.php
@@ -12,9 +12,11 @@
         <input type = "number" name = "uplimit" size = "20" maxlength = "20"><br> 
         <input type = "submit" value = "送信">
         <input type = "reset" value = "クリア">
-      　<a href="index.php">もどる</a>
-        <a href="input.php">入力する</a>
     </form>
+    
+    <button onclick="location.href='input.php'">入力する</button>
+    
+    <button onclick="location.href='index.php'">戻る</button>
     
   </body>
 
