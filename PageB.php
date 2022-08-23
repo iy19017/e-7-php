64aced37a314e1a7af34ee22b7666b48c7a8d9b6
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 12:53:03 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -4,6 +4,40 @@
 </head>
   <body>
       <?php
+// 	try{
+	  
+// 		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
+// 		$database = "dcrb5clh0jqmke";
+// 		$host = 5432;
+// 		$user_id = "owlsbojfhnbkxz";
+// 		$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
+
+// 		$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");	
+
+// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// 	} catch(PDOException $e){
+
+// 		echo "ERROR";
+
+// 	}  
+	
+	$shohin = $_POST['name'];
+	$kingaku = $_POST['money'];
+	$today = date('Y-m-d');
+	print("$shohin $kingaku $today");
+	?>
+  <br>
+  <form  method = "POST">
+	 <input type="submit" name="yes" value="よかった">
+	 <input type="submit" name="no" value="わるかった">
+  </form>
+</body>
+</html>
+  <?php
+
+  if(isset($_POST['yes'])) {
+	  
 	try{
 	  
 		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
@@ -21,22 +55,6 @@
 		echo "ERROR";
 
 	}  
-	
-	$shohin = $_POST['name'];
-	$kingaku = $_POST['money'];
-	$today = date('Y-m-d');
-	print("$shohin $kingaku $today");
-	?>
-  <br>
-  <form  method = "POST">
-	 <input type="submit" name="yes" value="よかった">
-	 <input type="submit" name="no" value="わるかった">
-  </form>
-</body>
-</html>
-  <?php
-
-  if(isset($_POST['yes'])) {
 	  
 	
 	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES ('".$shohin."', '".$kingaku."', '".data('Y-m-d')."');";
