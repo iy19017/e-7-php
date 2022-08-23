fc2211d76b1b1eb2c059b5eef0ba0bb9aee3d8b9
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 21:11:17 +0900

    Update PageB2.php

--- PageB2.php
+++ PageB2.php
@@ -1,17 +1,3 @@
-<?php
-	session_start();
-	$shohin = $_SESSION['name'];
-	$kingaku = $_SESSION['money'];
-
-	include './connect.php';
-	
-	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES ('".$shohin."',".$kingaku.", '".date('Y-m-d')."');";
-	  
-	$stmt = $pdo -> query($sql);
-
-	print ("商品名：${shohin} 価格：${kingaku} を登録しました");
-
-?>
 <html>	
 <head>
   <meta charset = "utf-8">
@@ -19,22 +5,24 @@
   <body>
 	 
   <?php
+	  
+  session_start();
+  $shohin = $_SESSION['name'];
+  $kingaku = $_SESSION['money'];
+
+  include './connect.php';
 	
-//   print ("商品名：${shohin} 価格：${kingaku} を登録しました");
+  $sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES ('".$shohin."',".$kingaku.", '".date('Y-m-d')."');";
+	  
+  $stmt = $pdo -> query($sql);
+	
+  print ("商品名：${shohin} 価格：${kingaku} を登録しました");
 
   ?>
 	  
   <form action="index.php">
 	 <input type="submit" name="return" value="もどる">
   </form>
+	  
 </body>
 </html>
-  <?php
-
-// 	include './connect.php';
-	
-// 	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES ('".$shohin."',".$kingaku.", '".date('Y-m-d')."');";
-	  
-// 	$stmt = $pdo -> query($sql);
-	  
-  ?>  
