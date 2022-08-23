234c7649f100332d6caf240221a4dd4b16e70ae0
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 17:22:00 +0900

    Update PageB2.php

--- PageB2.php
+++ PageB2.php
@@ -7,84 +7,36 @@
 	session_start();
 	$shohin = $_SESSION['name'];
 	$kingaku = $_SESSION['money'];
-	print("$shohin $kingaku");
-	?>
-  <br>
-<!--   <form medhod="post">
+  ?>
+  <form action="index.php">
 	 <input type="submit" name="yes" value="よかった">
-	 <input type="submit" name="no" value="わるかった">
-  </form> -->
+  </form>
 </body>
 </html>
   <?php
 
-//   try{
+	try{
 	  
-// 		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
-// 		$database = "dcrb5clh0jqmke";
-// 		$host = 5432;
-// 		$user_id = "owlsbojfhnbkxz";
-// 		$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
+		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
+		$database = "dcrb5clh0jqmke";
+		$host = 5432;
+		$user_id = "owlsbojfhnbkxz";
+		$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
 
-// 		$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");	
+		$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");	
 
-// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
-// 		echo "OK";
+		echo "OK";
 		
-// 	} catch(PDOException $e){
+	} catch(PDOException $e){
 
-// 		echo "ERROR";
+		echo "ERROR";
 		
-// 	}
-
-//   if(isset($_POST['yes'])) {
-
-// 	try{
-	  
-// 		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
-// 		$database = "dcrb5clh0jqmke";
-// 		$host = 5432;
-// 		$user_id = "owlsbojfhnbkxz";
-// 		$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
-
-// 		$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");	
-
-// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
-// 		echo "OK";
-		
-// 	} catch(PDOException $e){
-
-// 		echo "ERROR";
-		
-// 	}  
-	  
-// 	$shohin = $_GET['name'];
-// 	$kingaku = $_GET['money'];
-// 	$today = date('Y-m-d');
-	  
-// 	print("$shohin $kingaku $today");
+	}  
 	
-// 	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES ('".$_POST['name']."',".$_POST['money'].", '".date('Y-m-d')."');";
+	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES ('".$shohin."',".$kingaku.", '".date('Y-m-d')."');";
 	  
-//  	$stmt = $pdo->prepare($sql);
+	$stmt = $pdo -> query($sql);
 	  
-// 	echo "0";
-	  
-// 	$stmt = $pdo -> query($sql);
-	  
-// 	echo "1";
-	  
-// 	$stmt -> execute();
-	  
-// 	print("2");
-	  
-//   }
-	  
-//   else if(isset($_POST['no'])){
-	
-// 	header("Location: ./index.php") ;
-  
-//   }
   ?>  
