e622a03f0d3569ff99cf89353456f14a4884831d
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 22:11:03 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,19 @@
   </head>
     
   <body>
+    <?php
     
+    session_start();
+    
+    $check = $_SESSION['error'];
+    
+    If($check == true){
+    
+      print("不正な入力");
+      
+    }
+    
+    ?>
     <button onclick="location.href='input.php'">入力</button>
     
     <button onclick="location.href='search.php'">検索</button>
