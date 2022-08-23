8a9b3a9f72af7191ff63522877e22851670bb145
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 15:35:20 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -62,7 +62,7 @@
 	  
 	print("$shohin $kingaku $today");
 	
-	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES ('".$shohin."',".$kingaku.", '".date('Y-m-d')."');";
+// 	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES ('".$shohin."',".$kingaku.", '".date('Y-m-d')."');";
 	  
 //  	$stmt = $pdo->prepare($sql);
 	  
@@ -74,7 +74,7 @@
 	  
 	echo "0";
 	  
-	$stmt = $pdo -> query($sql);
+// 	$stmt = $pdo -> query($sql);
 	  
 	echo "1";
 	  
