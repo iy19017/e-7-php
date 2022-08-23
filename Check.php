0aef584428b3b1a74973cf0903e9c03034121658
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 22:01:48 +0900

    Create Check.php

---
+++ Check.php
@@ -0,0 +1,53 @@
+<?php
+
+  function ValueCheck($ItemPrice){
+  
+    if(($ItemPrice < 0) or (empty($ItemPrice)) or ($ItemPrice > 2147483647)){
+      return TRUE;
+    }
+    
+    return FALSE;
+  
+  }
+
+  function ValueRangeCheck($ItemPrice){
+  
+    if(($ItemPrice < 0) or ($ItemPrice > 2147483647)){
+      return TRUE;
+    }
+    
+    return FALSE;
+  
+  }
+
+  function InputNameCheck($ItemName){
+
+    if((SyntaxCheck($ItemName)) or (empty($ItemName))){
+      return TRUE;
+    }
+    
+    return FALSE;
+    
+  }
+
+  function SearchNameCheck($ItemName){
+  
+    if((SyntaxCheck($ItemName))){
+      return TRUE;
+    }
+    
+    return FALSE;
+  
+  }
+
+  function SyntaxCheck($ItemName){
+    $errorSyntax = "/<head|head>|<body|body>|<form|form>|<input|<br>/";
+    
+    if(preg_match($errorSyntax, $ItemName)){
+      return TRUE;
+    }
+    
+    return FALSE;
+  }
+
+?>
