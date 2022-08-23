007b37a5c7c7c1b4625f7fc667ad4fb7901df6c0
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 13:36:10 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -43,12 +43,12 @@
 		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
 		$database = "dcrb5clh0jqmke";
 		$host = 5432;
-		$user_id = "owlsbojfhnbkxz";
+		$user_id = "";
 		$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
 
 		$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");	
 
-// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
 // 		$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES ('".$shohin."', '".$kingaku."', '".data('Y-m-d')."');";
 
