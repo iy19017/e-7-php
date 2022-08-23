68b9e3857caca0af60fa929186919ae5076e1ca3
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 14:47:17 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -18,9 +18,7 @@
 </html>
   <?php
 
-  if(isset($_POST['yes'])) {
-
-	try{
+  try{
 	  
 		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
 		$database = "dcrb5clh0jqmke";
@@ -32,11 +30,35 @@
 
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
+		echo "OK";
+		
 	} catch(PDOException $e){
 
 		echo "ERROR";
 		
-	}  
+	}
+
+  if(isset($_POST['yes'])) {
+
+// 	try{
+	  
+// 		$server = "ec2-54-76-43-89.eu-west-1.compute.amazonaws.com";
+// 		$database = "dcrb5clh0jqmke";
+// 		$host = 5432;
+// 		$user_id = "owlsbojfhnbkxz";
+// 		$user_password = "743232ea27dee0cbb00552fac7d502b2f4a0aa4c7763af9193bc9b5d539ab51e"; 
+
+// 		$pdo = new PDO("pgsql:host=$server; dbname=$database; user=$user_id; password=$user_password");	
+
+// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+// 		echo "OK";
+		
+// 	} catch(PDOException $e){
+
+// 		echo "ERROR";
+		
+// 	}  
 	  
 	print("$shohin $kingaku $today");
 	
