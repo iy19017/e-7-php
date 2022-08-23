4f5be4a334e22a946dda688686cdbe8a02121f03
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-08-23 13:24:40 +0900

    Update PageB.php

--- PageB.php
+++ PageB.php
@@ -60,8 +60,9 @@
 		
 	}  
 	  
+	print("$shohin $kingaku $today");
 	
-	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES (':shohin', ':kingaku', ':today');";
+	$sql = "INSERT INTO \"order\" (商品名, 価格, 登録日) VALUES (\':shohin\', \':kingaku\', \':today\');";
 	  
  	$stmt = $pdo->prepare($sql);
 	  
